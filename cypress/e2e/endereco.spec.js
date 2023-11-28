3f0bd093401320b27f58f888e9ba3f1c5759c9bc
Adriana Campos <adrianacampossantos1902@gmail.com>
2023-11-27 21:28:21 -0300

    edições em endereço de entrega e faturamento

--- cypress/e2e/endereco.spec.js
+++ cypress/e2e/endereco.spec.js
@@ -1,19 +1,53 @@
 /// <reference types="cypress" />
+import enderecoPage from '../support/page-objects/endereco.page';
+import EnderecoPage from '../support/page-objects/endereco.page';
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
         cy.visit('minha-conta')
         cy.fixture('perfil').then(dados =>{
             cy.login(dados.usuario, dados.senha)
-        })
-       
+        }) 
     });
     
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-       
-       //cadastro de endereço 
+       EnderecoPage.editarEnderecoFaturamento('Adriana','Campos','EBAC','Brasil','Av Brasil','320','Belo Horizonte','Minas Gerais','01000100','31999998888','adriana@teste.com')
+       cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email)
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+     });
+    
+     it('Deve fazer cadastro de endereço de envio com sucesso', () => {
+        EnderecoPage.editarEnderecoEntrega('Adriana', 'Campos', 'Google', 'Brasil', 'Rua Maria Flor', '55', 'Belo Horizonte', 'Minas Gerais', '01000100')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+     });
 
-
+     it.only('Deve fazer cadastro de endereço de envio com sucesso', () => {
+        EnderecoPage.editarEnderecoEntrega(
+            dadosEndereco[2].nome,
+            dadosEndereco[2].sobrenome,
+            dadosEndereco[2].empresa,
+            dadosEndereco[2].pais,
+            dadosEndereco[2].rua,
+            dadosEndereco[2].numero,
+            dadosEndereco[2].cidade,
+            dadosEndereco[2].estado,
+            dadosEndereco[2].cep)
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+     });
 });
